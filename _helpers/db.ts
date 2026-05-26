3baa199bcf2e9f370330bca5600fd04e12bd5bfc
Irien Pepito <irienpepito@gmail.com>
2026-05-26 15:38:03 +0800

    use environment variables for db config

--- _helpers/db.ts
+++ _helpers/db.ts
@@ -1,5 +1,14 @@
 import { Sequelize } from 'sequelize';
-const config = require('../config.json');
+const config = {
+  database: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  },
+  secret: process.env.SECRET
+};
 
 export const db: any = {};
 
